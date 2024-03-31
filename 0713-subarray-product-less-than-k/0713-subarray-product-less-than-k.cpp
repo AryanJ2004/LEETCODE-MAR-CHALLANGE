3f75ea05d06ee9113ea52bfec5a8175da0e3a272
Aryan N. Jain <aryanj1084@gmail.com>
2024-03-31 10:40:54 +0530

    Time: 58 ms (52.61%), Space: 63.6 MB (90.1%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-        if (k == 0)
-            return 0;
-        int n = nums.size(), product = 1, cnt {};
-        int l = 0, r = 0;
-        while(r < n)
-        {
-            product *= nums[r++];
-            while(l < r && product >= k)
-                product /= nums[l++];
+        int n = nums.size();
+        long long product = 1;
+        int end=0;
+        int start=0;
+        int ans=0;
+        while(end<n && start<n){
+            product = product*nums[end];
+           
             
-            cnt += (r - l);
+            while(start<=end && product>=k){
+                product = product/nums[start];
+                start++;
+            }
+            
+            
+            ans = ans+end-start+1;
+            end++;
         }
-        return cnt;
+
+        return ans;
+        
     }
 };
